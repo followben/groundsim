d3470529681610a3ad2cc3b77cb034b3319b41f6
Ben Stovold <bstovold@gmail.com>
2022-10-26 20:57:56 +1000

    Rework simulator with basic graphql endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,44 @@
 import asyncio
 import json
-from typing import AsyncGenerator, Literal, Mapping, Optional
+import os
+from enum import Enum
+from typing import AsyncGenerator, Mapping, Optional
 
 import strawberry
-from fastapi import FastAPI
+from broadcaster import Broadcast
+from fastapi import BackgroundTasks, FastAPI
 from pydantic import BaseModel
 from strawberry.fastapi import GraphQLRouter
 
+BROADCAST_URL = os.environ.get("BROADCAST_URL", "memory://")
 
-class TrackingPoint(BaseModel):
-    type: Literal["az", "el", "power"]
+broadcast = Broadcast(BROADCAST_URL)
+
+
+@strawberry.enum
+class GSPointType(str, Enum):
+    az = "az"
+    el = "el"
+    power = "power"
+
+
+class GSPoint(BaseModel):
+    type: GSPointType
     value: float
 
 
-class TrackingPoints(BaseModel):
-    __root__: Mapping[int, list[TrackingPoint]]
+class GSTelemetry(BaseModel):
+    __root__: list[GSPoint]
+
+    def __iter__(self):
+        return iter(self.__root__)
+
+    def __getitem__(self, item):
+        return self.__root__[item]
+
+
+class SimulationDict(BaseModel):
+    __root__: Mapping[int, GSTelemetry]
 
     def __iter__(self):
         return iter(self.__root__)
@@ -26,20 +50,37 @@ class TrackingPoints(BaseModel):
         return self.__root__.keys()
 
 
+running: bool = False
+current: Optional[GSTelemetry] = None
+
+
+@strawberry.experimental.pydantic.type(model=GSPoint, all_fields=True)
+class GQLPoint:
+    ...
+
+
+@strawberry.experimental.pydantic.type(model=GSTelemetry, all_fields=True)
+class GQLTelemetry:
+    ...
+
+
+def get_latest() -> list[GQLPoint]:
+    return [GQLPoint.from_pydantic(p) for p in current] if current else []
+
+
 @strawberry.type
 class Query:
-    @strawberry.field
-    def hello(self) -> str:
-        return "bob"
+    latestpoints: list[GQLPoint] = strawberry.field(resolver=get_latest)
 
 
 @strawberry.type
 class Subscription:
     @strawberry.subscription
-    async def count(self, target: int = 100) -> AsyncGenerator[int, None]:
-        for i in range(target):
-            yield i
-            await asyncio.sleep(0.5)
+    async def points(self) -> AsyncGenerator[GQLPoint, None]:
+        async with broadcast.subscribe(channel="telemetry") as subscriber:
+            async for event in subscriber:
+                point = GSPoint.parse_raw(event.message)
+                yield GQLPoint.from_pydantic(point)
 
 
 schema = strawberry.Schema(query=Query, subscription=Subscription)
@@ -47,30 +88,28 @@ schema = strawberry.Schema(query=Query, subscription=Subscription)
 graphql_app = GraphQLRouter(schema)
 
 
-class AntennaSim(object):
+async def simulate_overpass():
+    global running, current
+    if running:
+        return
+    running = True
 
-    value: Optional[TrackingPoint] = None
-    _points: Optional[TrackingPoints] = None
-    _offset = 0
+    with open("tracking.json", "r") as f:
+        data = SimulationDict.parse_obj(json.load(f))
 
-    @property
-    def points(self) -> TrackingPoints:
-        if not self._points:
-            with open("tracking.json", "r") as f:
-                self._points = TrackingPoints.parse_obj(json.load(f))
-        return self._points
+    counter = 0
+    emit_at = data.keys()
 
-    async def run_main(self):
-        while True:
-            if self.points:
-                if self._offset in self.points.keys():
-                    new_points = self.points[self._offset]
-                    self.value = new_points[0]
-            await asyncio.sleep(1)
-            self._offset += 1
+    while counter <= max(emit_at):
+        if counter in emit_at:
+            current = data[counter]
+            for point in current:
+                await broadcast.publish(channel="telemetry", message=point.json())
+        await asyncio.sleep(1)
+        counter += 1
 
+    current = None
 
-sim = AntennaSim()
 
 app = FastAPI()
 
@@ -79,10 +118,19 @@ app.include_router(graphql_app, prefix="/graphql")
 
 @app.on_event("startup")
 async def app_startup():
-    asyncio.create_task(sim.run_main())
+    await broadcast.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    await broadcast.disconnect()
 
 
 @app.get("/")
-def root():
-    result = sim.value.dict() if sim.value else None
-    return result
+async def get_current():
+    return current
+
+
+@app.post("/simulation")
+async def create_simulation(background_tasks: BackgroundTasks):
+    background_tasks.add_task(simulate_overpass)
