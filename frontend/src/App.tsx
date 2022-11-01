b292209eb1d3a0fe8e1f0f96e3c24b9727112061
Ben Stovold <bstovold@gmail.com>
2022-11-01 19:38:26 +1000

    Enable graphql for local frontend dev

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,41 @@
+import { MantineProvider } from '@mantine/core';
 import { useState } from 'react';
 import './App.css';
 
 import { Button } from '@mantine/core';
+import { ClientContext, GraphQLClient, useMutation } from 'graphql-hooks';
 
-function App() {
-  const [count, setCount] = useState(0)
+const CREATE_SIMULATION_MUTATION = `mutation {
+  createSimulation
+}`
 
+const client = new GraphQLClient({
+  url: 'http://localhost:8080/graphql'
+})
+
+function Dashboard() {
+  const [created, setCreated] = useState(false)
+  const [createSimulation] = useMutation(CREATE_SIMULATION_MUTATION)
   return (
     <div className="App">
       <h1>Groundsim</h1>
-      <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <Button onClick={async () => {
+        const { data: { createSimulation: success } } = await createSimulation()
+        setCreated(success)
+      }}>
+          created is {created ? "true" : "false"}
       </Button>
     </div>
+  )
+}
+
+function App() {
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme: 'dark' }}>
+      <ClientContext.Provider value={client}>
+        <Dashboard/>
+      </ClientContext.Provider>
+    </MantineProvider>
   )
 }
 
